526f3afb8ff53d76feef04aa1ab3464372548377
Dér Zsombor <zsomborder@gmail.com>
2021-12-11 13:33:13 +0100

    Renamed some main files and added comments

--- List/examples.c
+++ List/examples.c
@@ -1,23 +1,31 @@
 #include "List.h"
 #include <stdio.h>
 int main(){
+//Creates a new list
 List list;
-
 InitList(&list);
+
+//Adds the elements into the list
 Add(&list,10);
 Add(&list,15);
 Add(&list,17);
 Add(&list,55);
 Add(&list,143);
+
+//Writes out the list's contents
+for (int i = 0; i < Count(&list); i++)
+{
+  //Get the element at the i index and print it
+  printf("%d \n",GetItem(&list,i));
+}
+//Remove elements at the 3 index
+RemoveAt(&list,3);
+RemoveAt(&list,3);
+//Writes out the list's contents
 for (int i = 0; i < Count(&list); i++)
 {
   printf("%d \n",GetItem(&list,i));
 }
-RemoveAt(&list,3);
-RemoveAt(&list,3);
-for (int i = 0; i < Count(&list); i++)
-{
-  printf("%d \n",GetItem(&list,i));
-}
+//Deletes the list and frees up the memory
 DeleteList(&list);
 }
--- StringMethods/examples.c
+++ StringMethods/examples.c
@@ -5,25 +5,29 @@
 //Examples
 int main(){
 
+//The example string
+//(it has to be of type char[] with char* it doesn't work)
 char arr[] ="172.25.62.7";
 
+//Gets the number of characters in the string
 printf("Size of string: %d\n", GetStringLength(arr));
 
+//Replaces the string's '4' characters to '8'
 printf("OG string: %s \n",arr);
 Replace(arr,'4','8');
-
 printf("Replaced string: %s\n",arr);
 
+//Checks if the string contains '.'
 printf("Contains: %d\n",Contains(arr,"."));
 
+//Gets the substring after second '.' till the third '.'
 char splitted[4];
 Cut(arr,".",2,splitted);
-
 printf("Splitted: %s\n",splitted);
 
+//Creates a substring from the third character till the end of the string
 char subStr[20];
 SubString(arr,subStr,2,-1);
-
 printf("Substring: %s",subStr);
 
     return 0;
