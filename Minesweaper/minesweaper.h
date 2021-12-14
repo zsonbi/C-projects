143690c025c4bebd7698208f2c2cbd07887a5568
Dér Zsombor Zsolt <der.zsombor.zsolt@burg.lo.hu>
2021-12-14 13:30:49 +0100

    Started minesweaper

---
+++ Minesweaper/minesweaper.h
@@ -0,0 +1,21 @@
+#ifndef MINESWEAPER_H_INCLUDED
+#define MINESWEAPER_H_INCLUDED
+
+struct Game{
+
+}
+
+#typedef struct Game Game
+
+
+
+
+void NewGame(int xSize, int ySize, int mineCount){
+
+}
+
+
+
+
+
+#endif
